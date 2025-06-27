76c1c44f44a7f28b2e7d06fc6c97b68af2d1613b
dmitrijsuhanov713@gmail.com <dmitrijsuhanov713@gmail.com>
2025-06-27 14:51:03 +0300

    Добавьте файлы проекта.

---
+++ TaskScheduler/Models/TaskChecklist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskScheduler.Models
+{
+    public class TaskChecklist
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
